8d7353689276e455f6dfa7740b6079c02cbe8351
silver-sh96 <cubha@naver.com>
2023-05-24 11:16:43 +0900

    axios로 backend와 통신

--- src/pages/NoticeList.tsx
+++ src/pages/NoticeList.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const NoticeList = () => {
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/apiTest')
+        .then((Response)=>{
+            console.log(Response.data);
+        }).catch((Error)=>{
+            console.log(Error);
+        })
+    }, [])
+    
     return (
         <div>
             나는리스트
